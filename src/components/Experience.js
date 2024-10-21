e546213cfa6347ef1a016e53bdf5e33aae4babf9
Jaishree Saraswat <saraswatjaishree.14@gmail.com>
2024-10-21 22:10:26 +0200

    add all changes

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,6 +5,8 @@ function Experience() {
   return (
     <section id="experience" className="experience">
     
+
+    
       <h2 className="section-title">Experience</h2>
       <div className="timeline">
         <div className="timeline-item">
@@ -12,20 +14,11 @@ function Experience() {
           <div className="timeline-content">
             <h4>Analyst</h4>
             <p>Deloitte</p>
-            <p>Managed 15+ servers in a critical Azure infrastructure with MySQL databases. Established Azure CI/CD pipelines using DevOps YAML, configuring build and release stages. Added tasks to build Docker images, ensuring proper authentication and specifications, enhancing deployment efficiency and reliability. Investigated and resolved defects, ensuring smooth operations and continuous improvements.</p>
+            <p>• Developed and deployed AI-driven data solutions using Python, optimizing data pipelines for efficient knowledge management. <br></br>• Collaborated with cross-functional teams to understand business requirements and implement AI-based models for enhanced decision-making and customer interactions. <br></br>• Leveraged Azure technologies (including Event Hub, Data Lake, and Data Factory) to build scalable solutions, integrating AI capabilities and supporting real-time data processing. <br></br>• Implemented chatbot frameworks and integrated NLP models for automating customer interactions, demonstrating initial experience with LLM technologies. <br></br>• Documented AI and data solutions, creating user-friendly guides and instructions for deployment and maintenance to ensure seamless knowledge transfer. <br></br>• Maintained efficient version control using Git and CI/CD tools like Jenkins to deploy solutions in production environments, ensuring system robustness. <br></br>• Optimized Python scripts for AI model integration, employing techniques like data partitioning and caching to enhance model performance. <br></br>• Gained hands-on experience in data migration and ETL processes, providing solutions that align with AI and machine learning requirements for large-scale enterprise projects.</p>
           </div>
         </div>
-
-        <div className="timeline-item">
-          <div className="timeline-date">2022</div>
-          <div className="timeline-content">
-            <h4>Software Engineer - Intern</h4>
-            <p>Deloitte</p>
-            <p>Developed and implemented a web application, optimized performance, and ensured seamless AWS/Azure deployment. Monitored application performance with Application Insights, streamlined change management with ServiceNow, and actively participated in Agile meetings.</p>
-          </div>
-        </div>
-        </div>
-
+      </div>
+      
         <h2 className="section-title">Education</h2>
         <div className="timeline">
             <div className="timeline-item">
@@ -35,7 +28,8 @@ function Experience() {
                 <p>TU Ilmenau, Germany</p>
                 <p>Core Subjects - Advance Database Systems, Algorithms, Advanced Mobile Communication Networks, Software and Systems Engineering, Control Engineering. Advance studies included Machine Learning, Deep Learning, Data Storage Systems, Distributed Sytems.</p>
             </div>
-            </div>
+            </div> 
+            
 
             <div className="timeline-item">
             <div className="timeline-date">2018 - 2022</div>
